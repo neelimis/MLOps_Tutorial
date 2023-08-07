d49f8697b5c771e35fd56ed57c537b6b1669c1c4
neelimis <94392208+neelimis@users.noreply.github.com>
2023-08-07 09:55:32 +0100

    Update train.py
    challenge 0 - added function to split data and enable auto logging

--- src/model/train.py
+++ src/model/train.py
@@ -3,15 +3,16 @@
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    mlflow.autolog()
 
 
     # read data
@@ -35,6 +36,9 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 
+def split_data(df):
+    return X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
